ec3053b0aa99a715eea39722022f08e0e4c01ad9
sugizou3 <sugiyama20010501@gmail.com>
2026-05-19 13:57:34 +0900

    htmlのひな形を作成
    exam01.htmlの実装完了
    
    exam01-result.htmlの実装完了
    
    htmlのフォーム実装、コントローラーの実装完了

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,22 +1,24 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 
 public class Exam01Controller {
-    @GetMapping("")
-    public String index() {
+    @GetMapping("/")
+    public String index(Model model) {
         return "exam01";
     }
 
-    @PostMapping("name-post")
-    public String namePost() {
-        return "exam01-result.html";
+    @PostMapping("/name-post")
+    public String namePost(String name, Model model) {
+        model.addAttribute("name", name);
+        return "exam01-result";
     }
-    
+
 }
