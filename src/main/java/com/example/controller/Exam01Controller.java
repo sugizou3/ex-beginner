61da40208ecf162efd5dcdf4bdef86d9f304fd6b
sugizou3 <sugiyama20010501@gmail.com>
2026-05-19 13:54:11 +0900

    コントローラーのひな形を作成

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,4 +1,22 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+
 public class Exam01Controller {
+    @GetMapping("")
+    public String index() {
+        return "exam01";
+    }
+
+    @PostMapping("name-post")
+    public String namePost() {
+        return "exam01-result.html";
+    }
+    
 }
